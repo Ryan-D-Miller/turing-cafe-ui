9190069091daca524c4d25a4e7d345bbcd82621a
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-26 09:24:39 -0600

    Add fetch call for reservations

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+const reservations = "http://localhost:3001/api/v1/reservations";
+
+const getReservations = () => {
+    return fetch(reservations)
+        .then(res => {
+            if(res.ok) {
+                return res.json()
+            }
+            throw new Error(res)
+        })
+}
